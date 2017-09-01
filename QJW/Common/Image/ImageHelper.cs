0859d617b99598924b9798bb60921b9799c9e6c9
阿炬 <quejuwen@gmail.com>
2017-09-01 17:50:10 +0800

    增加图像截取方法

--- QJW/Common/Image/ImageHelper.cs
+++ QJW/Common/Image/ImageHelper.cs
@@ -591,5 +591,66 @@ namespace QJW.Common
             }
         }
         #endregion
+
+
+        #region 从大图中截取一部分图片
+        /// <summary>
+        /// 从大图中截取一部分图片
+        /// </summary>
+        /// <param name="fromImagePath">来源图片地址</param>        
+        /// <param name="offsetX">从偏移X坐标位置开始截取</param>
+        /// <param name="offsetY">从偏移Y坐标位置开始截取</param>
+        /// <param name="toImagePath">保存图片地址</param>
+        /// <param name="width">保存图片的宽度</param>
+        /// <param name="height">保存图片的高度</param>
+        /// <returns></returns>
+        public static void CaptureImage(string fromImagePath, int offsetX, int offsetY, string toImagePath, int width, int height)
+        {
+            //原图片文件
+            Image fromImage = Image.FromFile(fromImagePath);
+            //创建新图位图
+            Bitmap bitmap = new Bitmap(width, height);
+            //创建作图区域
+            Graphics graphic = Graphics.FromImage(bitmap);
+            //截取原图相应区域写入作图区
+            graphic.DrawImage(fromImage, 0, 0, new Rectangle(offsetX, offsetY, width, height), GraphicsUnit.Pixel);
+            //从作图区生成新图
+            Image saveImage = Image.FromHbitmap(bitmap.GetHbitmap());
+            //保存图片
+            saveImage.Save(toImagePath, ImageFormat.Png);
+            //释放资源   
+            saveImage.Dispose();
+            graphic.Dispose();
+            bitmap.Dispose();
+        }
+        #endregion
+
+
+        /// <summary>
+        /// get a certain rectangle part of a known graphic
+        /// </summary>
+        /// <param name="bitmapPathAndName">path and name of the source graphic</param>
+        /// <param name="width">width of the part graphic</param>
+        /// <param name="height">height of the part graphic</param>
+        /// <param name="offsetX">the width offset in the source graphic</param>
+        /// <param name="offsetY">the height offset in the source graphic</param>
+        /// <returns>wanted graphic</returns>
+        public static void GetPartOfImage(string bitmapPathAndName, int offsetX, int offsetY,string toImagePath, int width, int height)
+        {
+            Bitmap sourceBitmap = new Bitmap(bitmapPathAndName);
+            Bitmap resultBitmap = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(resultBitmap))
+            {
+                Rectangle resultRectangle = new Rectangle(0, 0, width, height);
+                Rectangle sourceRectangle = new Rectangle(0 + offsetX, 0 + offsetY, width, height);
+                g.DrawImage(sourceBitmap, resultRectangle, sourceRectangle, GraphicsUnit.Pixel);
+            }
+            resultBitmap.Save(toImagePath, ImageFormat.Png);
+            sourceBitmap.Dispose();
+            resultBitmap.Dispose();
+        }
+
+
+
     }
 }
